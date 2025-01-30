b251b5e406f3fc5bfcdcca785f69cbc5247de814
Kapil Jugnu <kr_kapil@hotmail.com>
2025-01-30 12:16:56 +0530

    make reusable component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { HeaderComponent } from './header/header.component';
 import { ServerStatusComponent } from './dashboard/server-status/server-status.component';
 import { TrafficComponent } from './dashboard/traffic/traffic.component';
 import { TicketComponent } from './dashboard/ticket/ticket.component';
+import { DashboardItemComponent } from './dashboard/dashboard-item/dashboard-item.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, ServerStatusComponent, TrafficComponent, TicketComponent],
+  imports: [HeaderComponent, ServerStatusComponent, TrafficComponent, TicketComponent, DashboardItemComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
