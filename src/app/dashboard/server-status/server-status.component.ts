553e81bfba2f3ba50f4bf3cb3bf89bde389319d5
Kapil Jugnu <kr_kapil@hotmail.com>
2025-02-01 05:35:06 +0530

    ngOnDestroy lifecycle

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
@@ -7,11 +7,13 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css',
 })
-export class ServerStatusComponent implements OnInit {
+export class ServerStatusComponent implements OnInit, OnDestroy {
+  
   currentStatus: 'online' | 'offline' | 'unknown' = 'online';
+  private interval?: ReturnType<typeof setInterval>;
 
   ngOnInit() {
-    setInterval(() => {
+    this.interval = setInterval(() => {
       const rnd = Math.random(); // 0 - 0.999999999
       if (rnd < 0.5) {
         this.currentStatus = 'online';
@@ -22,4 +24,9 @@ export class ServerStatusComponent implements OnInit {
       }
     }, 5000);
   }
+
+  ngOnDestroy(): void {
+    clearInterval(this.interval)
+  }
+  
 }
