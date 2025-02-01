8f371dd02822d9ee5336f54752a14d8f023f6cd3
Kapil Jugnu <kr_kapil@hotmail.com>
2025-02-01 05:47:18 +0530

    accessing template variable value

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -11,7 +11,7 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './new-ticket.component.css'
 })
 export class NewTicketComponent {
-  onSubmit(titleInput: HTMLInputElement) {
-    console.log(titleInput.value)
+  onSubmit(title: string, text: string) {
+    console.log(title, text)
   }
 }
